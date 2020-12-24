6bf15ea3224a6a3b37c5224f44ebbeac01f1b581
Dillan Tai <dillantai@gmail.com>
2020-12-24 00:45:05 -0800

    debug netlify form success

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,57 +1,66 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+        <meta
+          name="description"
+          content={"Contact page of " + site.siteMetadata.description}
+        />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div
+          className="post-thumbnail"
+          style={{
+            backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`,
+            marginBottom: 0
+          }}
+        >
           <h1 className="post-title">Get in Touch</h1>
         </div>
         <div>
           <form
             name="contact"
             method="post"
-            action="/contact-success"
+            action="/contact-success" //
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            className="form-container"
+            className="form-container" //
           >
-            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div>
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div>
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" id="email" />
             </div>
             <div>
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="6" required />
             </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <input
+                type="submit"
+                className="button -primary"
+                style={{ marginRight: 0 }}
+              />
             </div>
           </form>
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -59,4 +68,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
