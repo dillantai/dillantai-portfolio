ed3da6255df320281df38b9008e0b61bd00ebf78
Dillan Tai <dillantai@gmail.com>
2020-12-29 13:53:57 -0800

    abstract siteMetadata graphql title query into react hooks

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,30 @@
-import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import 'prismjs/themes/prism-okaidia.css';
+import React from "react";
+import { Link } from "gatsby"
+import Navigation from "../components/navigation";
+import "prismjs/themes/prism-okaidia.css";
+import useSiteMetadata from "../hooks/use-sitemetadata";
 
-export default ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
+const Layout = ({ children }) => {
+  const { title } = useSiteMetadata();
+
   return (
     <div className="site-wrapper">
       <header className="site-header">
         <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+          <Link to="/">{title}</Link>
         </div>
         <Navigation />
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} dillan tai. built with <a href="https://www.gatsbyjs.com/">gatsby</a> and deployed with <a href="https://www.netlify.com/">netlify</a>.</p>
+        <p>
+          &copy; {new Date().getFullYear()} dillan tai. built with{" "}
+          <a href="https://www.gatsbyjs.com/">gatsby</a> and deployed with{" "}
+          <a href="https://www.netlify.com/">netlify</a>.
+        </p>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Layout;
